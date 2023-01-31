995bca1a8f4510f3d1deca772b152e6ec687fc76
Aika C <aika.carriere@gmail.com>
2023-01-31 11:32:13 -0600

    webpack.config decomposition
    - split the webpack.config into buildLoaders, buildPlugins and buildResolvers
    - implemented the option of choosing the mode: production || development

---
+++ config/build/types/config.ts
@@ -0,0 +1,12 @@
+export type buildMode = 'production' | 'development';
+
+export interface BuildPaths {
+    entry: string;
+    build: string;
+    html: string;
+}
+export interface BuildOptions {
+    mode: buildMode;
+    paths: BuildPaths;
+    isDev: boolean;
+}
