fad1f4a471ef1d60e5aed7b349d44e279d241c51
Aika C <aika.carriere@gmail.com>
2023-02-01 13:29:01 -0600

    Added Routing, code splitting, Lazy, Suspense
    - added routing,added two pages. Now able to switch between two pages
    - added lazy loading that splits the code into chunks
    - added suspense in App.ysx that displays a fallback until its children have finished loading.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Counter } from './components/Counter';
+import { Route, Routes, Link } from 'react-router-dom';
 import './index.scss';
+
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { AboutMainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
     return (
         <div className='app'>
-            Hello Hello
-            <Counter />
+            <Link to='/about'>ABOUT</Link>
+            <br />
+            <Link to='/'>MAIN </Link>
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<AboutMainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
