219e766e8c0895dd36a8cf65c1fb922f28fb2838
Aika C <aika.carriere@gmail.com>
2023-02-06 10:50:52 -0600

    Re-Structure folders
    - restructured the folders and split the project into 6 folders/layers: app, entities, features, pages, shared and widgets
    - now the code is structured accordign to feature-sliced-design. The layers are vertically arranged, modules on one layer can only interact with modules from the layers strictly below.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState, Suspense, useContext } from 'react';
+import { Suspense } from 'react';
 
 import { Route, Routes, Link } from 'react-router-dom';
 import './styles/index.scss';
 
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { AboutMainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classnames/classNames';
+import { classNames } from 'shared/lib/classnames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 export enum Theme {
     LIGHT = 'light',
@@ -23,8 +23,8 @@ const App = () => {
             <Link to='/'>MAIN </Link>
             <Suspense fallback={<div>...Loading</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<AboutMainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
--- src/pages/MainPage/MainPage.async.tsx
+++
@@ -1,3 +0,0 @@
-import { lazy } from 'react';
-
-export const AboutMainPageAsync = lazy(() => import('./MainPage'));
