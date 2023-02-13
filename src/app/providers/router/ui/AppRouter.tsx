56e102670c3fdde7df063ce3829c01bf9569189d
Aika C <aika.carriere@gmail.com>
2023-02-13 13:53:47 -0600

    Add NotFoundPage component, NotFoundPage route, PageLoader and Lodaer components
    - Added NotFoundPage compomnent and route
    - added translations for not found page
    - added PageLoader module in widgets
    - added Loader moodule in shared layer

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,26 +1,25 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>...Loading</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({ path, element }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={
-                            <Suspense fallback={<div>Loading...</div>}>
-                                <div className='page-wrapper'>{element}</div>
-                            </Suspense>
-                        }
-                    />
-                ))}{' '}
-            </Routes>
-        </Suspense>
+        // <Suspense fallback={<PageLoader />}>
+        <Routes>
+            {Object.values(routeConfig).map(({ path, element }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={
+                        <Suspense fallback={<PageLoader />}>
+                            <div className='page-wrapper'>{element}</div>
+                        </Suspense>
+                    }
+                />
+            ))}{' '}
+        </Routes>
+        // </Suspense>
     );
 };
 
---
+++ src/shared/ui/Loader/ui/Loader.tsx
@@ -0,0 +1,20 @@
+import { classNames } from 'shared/lib/classnames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+    return (
+        <div className={classNames('loadingio-spinner-ellipsis-l5qqijt9br')}>
+            <div className={classNames('ldio-3m0w5j0l1z1')}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
