c3308971b9e65810a6f487900f4858d8a4badbdb
Aika C <aika.carriere@gmail.com>
2023-03-23 11:45:13 -0500

    Add global const __API__ on Build process
    - declare global const __API__ and add it to jest, storybook, webpack and types configs so they are aware of the const
    - define __API__ on Build , localhost || productionUrl

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,7 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 import axios from 'axios';
 
 export const $api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: __API__,
     headers: {
         authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY),
     },
--- webpack.config.ts
+++ webpack.config.ts
@@ -13,11 +13,13 @@ export default (env: BuildEnv) => {
     const mode = env.mode || 'development';
     const isDev = mode === 'development';
     const PORT = env.port || 3000;
+    const apiUrl = env.apiUrl || 'http://localhost:5000';
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
         paths,
         isDev: isDev,
         port: PORT,
+        apiUrl,
     });
     return config;
 };
