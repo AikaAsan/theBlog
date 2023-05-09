1294cbdbbe37a63f40265ec3bdaee254e0c78965
Aika C <aika.carriere@gmail.com>
2023-05-09 14:51:24 -0500

    create UI state feature that restores scroll position in src/features layer
    - create ui schema, slice and selectors

---
+++ src/features/UI/index.ts
@@ -0,0 +1,5 @@
+export { uiReducer, uiActions } from 'features/UI/model/slices/UISlice';
+
+export { getUIScrollPositionByPath } from './model/selectors/ui';
+
+export { UISchema } from './model/types/UISchema';
--- src/shared/ui/Page/Page.tsx
+++ src/shared/ui/Page/Page.tsx
@@ -1,7 +1,18 @@
-import { FC, MutableRefObject, ReactNode, useRef } from 'react';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { uiActions } from 'features/UI';
+import {
+    getUIScrollPosition,
+    getUIScrollPositionByPath,
+} from 'features/UI/model/selectors/ui';
+import { FC, MutableRefObject, ReactNode, useRef, UIEvent } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 import { classNames } from 'shared/lib/classnames/classNames';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
 import cls from './Page.module.scss';
 
 interface PageProps {
@@ -14,16 +25,35 @@ export const Page: FC<PageProps> = (props: PageProps) => {
     const { className, children, onScrollEnd } = props;
     const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
     const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const dispatch = useAppDispatch();
+    const { pathname } = useLocation();
+    const scrollPosition = useSelector((state: StateSchema) =>
+        getUIScrollPositionByPath(state, pathname)
+    );
 
     useInfiniteScroll({
         triggerRef,
         wrapperRef,
         callback: onScrollEnd,
     });
+
+    useInitialEffect(() => {
+        wrapperRef.current.scrollTop = scrollPosition;
+    });
+    const onScrollHandler = useThrottle((e: UIEvent<HTMLDivElement>) => {
+        dispatch(
+            uiActions.setScrollPosition({
+                position: e.currentTarget.scrollTop,
+                path: pathname,
+            })
+        );
+        console.log('scroll', e.currentTarget.scrollTop);
+    }, 500);
     return (
         <section
             ref={wrapperRef}
             className={classNames(cls.page, {}, [className])}
+            onScroll={onScrollHandler}
         >
             {children}
             <div ref={triggerRef} />
