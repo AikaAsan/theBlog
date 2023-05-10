50cdc29626d1ceb4e2beb55752d5b550a6431e98
Aika C <aika.carriere@gmail.com>
2023-05-10 09:37:40 -0500

    edit useThrottle hook and add clearTimeOut function

--- src/shared/lib/hooks/useThrottle/useThrottle.ts
+++ src/shared/lib/hooks/useThrottle/useThrottle.ts
@@ -1,20 +1,29 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 export function useThrottle(callback: (...args: any[]) => void, delay: number) {
     const throttleRef = useRef(false);
+    const timeoutRef = useRef<any>(null);
 
-    return useCallback(
-        (...args: any[]) => {
+    const throttledCallback = useCallback(
+        (...args) => {
             if (!throttleRef.current) {
                 callback(...args);
-
                 throttleRef.current = true;
 
-                setTimeout(() => {
+                timeoutRef.current = setTimeout(() => {
                     throttleRef.current = false;
                 }, delay);
             }
         },
         [callback, delay]
     );
+
+    useEffect(
+        () => () => {
+            clearTimeout(timeoutRef.current);
+        },
+        []
+    );
+
+    return throttledCallback;
 }
