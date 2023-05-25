4dc758eb8692a9806339ec405914903138eac6ab
Aika C <aika.carriere@gmail.com>
2023-05-25 15:00:04 -0500

    prepare fro Netlify deployment
    - installed copy webpack plugin to copy translations locales
    - deployed backend on [render.com](http://render.com) and add the link to the build command in package.json
    - removed all async kostyl from lazy files

--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
@@ -1,10 +1,4 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { lazy } from 'react';
 
-export const ArticleEditPageAsync = lazy(
-    () =>
-        new Promise((resolve) => {
-            // @ts-ignore
-            setTimeout(() => resolve(import('./ArticleEditPage')), 400);
-        })
-);
+export const ArticleEditPageAsync = lazy(() => import('./ArticleEditPage'));
