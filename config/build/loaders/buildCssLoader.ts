d6b5da626749c2f2cc1d7131900d7702c4aeadb2
Aika C <aika.carriere@gmail.com>
2023-05-29 11:54:09 -0500

    fix netlify buid error

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -19,7 +19,7 @@ export function buildCssLoader(isDev: boolean) {
                     },
                 },
             },
-            // 'sass-loader',
+            'sass-loader',
         ],
     };
 }
