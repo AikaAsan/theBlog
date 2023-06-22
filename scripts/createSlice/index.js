6e2708559d4ce82d1f41ebcd0168466bd067b77d
Aika C <aika.carriere@gmail.com>
2023-06-22 11:30:38 -0500

    add scripts that generate feature layer templates

---
+++ scripts/createSlice/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const createTemplate = require('./templates/createTemplate');
+
+const layer = process.argv[2];
+const sliceName = process.argv[3];
+
+process.argv.forEach((value) => console.log(value));
+
+const layers = ['features', 'entities', 'pages'];
+
+if (!layer || !layers.includes(layer)) {
+    throw new Error(`Укажите слой ${layers.join(' или ')}`);
+}
+
+if (!sliceName) {
+    throw new Error('Укажите название слайса');
+}
+
+createTemplate(layer, sliceName);
