50b6608a76cdeb15a7b8019e51d7c24bbd7578ac
Aika C <aika.carriere@gmail.com>
2023-06-28 12:13:50 -0500

    Create ArticleDetailsComment component
    - created ArticleDetailsComment in pages/ArticleDetailsPage layer. Moved Text, AddCommentForm, CommentList components from ArticleDetailsPage inside it
    - added ArticleDetailsComment component in ArticleDetailsPage. Now it looks lean without additional logic
    
     Please enter the commit message for your changes. Lines starting

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,31 +1,21 @@
 /* eslint-disable max-len */
-import { ArticleDetails, ArticleList } from 'entities/Article';
-import { FC, memo, useCallback } from 'react';
+import { ArticleDetails } from 'entities/Article';
+import { FC, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { classNames } from 'shared/lib/classnames/classNames';
 import cls from './ArticleDetailsPage.module.scss';
-import { Text, TextSize } from 'shared/ui/Text/ui/Text';
-import { CommentList } from 'entities/Comment';
 import {
     DynamicModuleLoader,
     ReducersList,
 } from 'shared/components/DynamicModuleLoader/DynamicModuleLoader';
-import { getArticleComments } from '../../model/slices/articleDetailsCommentsSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { getArticleDetailsCommentsisLoading } from '../../model/selectors/comments';
-import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import AddCommentForm from 'features/AddCommentForm/ui/AddCommentForm/AddCommentForm';
-import { addCommentForArticle } from '../../model/services/AddCommentForArticle/AddCommentForArticle';
 import { Page } from 'shared/ui/Page/Page';
-import { getArticleRecommendations } from '../../model/slices/articleDetailsPageRecommendationsSlice';
-import { getArticleRecommendationsIsLoading } from '../../model/selectors/recommendations';
-import { fetchArticleRecommendations } from '../../model/services/fetchArticleRecommendations/fetchArticleRecommendations';
+
 import { articleDetailsPageReducer } from '../../model/slices';
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import { VStack } from 'shared/ui/Stack';
 import { ArticlesRecommendationsList } from 'features/ArticlesRecommendationsList';
+import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
 
 const reducers: ReducersList = {
     articleDetailsPage: articleDetailsPageReducer,
@@ -38,21 +28,7 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = (props) => {
     const { className } = props;
     const { t } = useTranslation('article');
     const { id } = useParams<{ id: string }>();
-    const dispatch = useDispatch();
-    const comments = useSelector(getArticleComments.selectAll);
 
-    const commentsIsLoading = useSelector(getArticleDetailsCommentsisLoading);
-
-    const onSendCommentHandler = useCallback(
-        (text) => {
-            dispatch(addCommentForArticle(text));
-        },
-        [dispatch]
-    );
-
-    useInitialEffect(() => {
-        dispatch(fetchCommentsByArticleId(id));
-    });
     if (!id) {
         return (
             <Page
@@ -75,12 +51,7 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = (props) => {
                     <ArticleDetailsPageHeader />
                     <ArticleDetails id={id} />
                     <ArticlesRecommendationsList />
-                    <Text title={t('Comments')} className={cls.commentTitle} />
-                    <AddCommentForm onSendComment={onSendCommentHandler} />
-                    <CommentList
-                        comments={comments}
-                        isLoading={commentsIsLoading}
-                    />
+                    <ArticleDetailsComments id={id} />
                 </VStack>
             </Page>
         </DynamicModuleLoader>
