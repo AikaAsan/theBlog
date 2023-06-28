96516340be2c253bed4e2a28354425a1435786b2
Aika C <aika.carriere@gmail.com>
2023-06-28 12:44:08 -0500

    Create ArticleInfiniteList component in pages/ArticlesPage layer
    - created ArticleInfiniteList and moved ArticleList logic there
    - added ArticleInfiniteList in ArticlesPage
    - edited translations

---
+++ src/pages/ArticlesPage/ui/ArticleInfiniteList/ArticleInfiniteList.tsx
@@ -0,0 +1,49 @@
+import { ArticleList } from 'entities/Article';
+import {
+    getArticlesPageIsLoading,
+    getArticlesPageError,
+    getArticlesPageView,
+} from '../../model/selectors/articlePageSelectors';
+import { initArticlesPage } from '../../model/services/initArticlesPage/initArticlesPage';
+import { getArticles } from '../../model/slices/articlesPageSlice';
+import { FC, memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { Text } from 'shared/ui/Text/ui/Text';
+
+interface ArticleInfiniteListProps {
+    className?: string;
+}
+
+export const ArticleInfiniteList: FC<ArticleInfiniteListProps> = memo(
+    (props: ArticleInfiniteListProps) => {
+        const { className } = props;
+        const { t } = useTranslation('article');
+        const dispatch = useAppDispatch();
+
+        const articles = useSelector(getArticles.selectAll);
+        const isLoading = useSelector(getArticlesPageIsLoading);
+        const error = useSelector(getArticlesPageError);
+        const view = useSelector(getArticlesPageView);
+
+        const [searchParams] = useSearchParams();
+
+        useInitialEffect(() => {
+            dispatch(initArticlesPage(searchParams));
+        });
+        if (error) {
+            return <Text text={t("Could not load articles")} />;
+        }
+        return (
+            <ArticleList
+                isLoading={isLoading}
+                view={view}
+                articles={articles}
+                className={className}
+            />
+        );
+    }
+);
