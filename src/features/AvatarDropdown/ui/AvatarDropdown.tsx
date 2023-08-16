2178db5e27f05af95eefed569cc3a28e2c809aba
Aika C <aika.carriere@gmail.com>
2023-08-16 11:43:22 -0500

    broke down Navbar widget, created AvatarDropdown and NotificationButton features and used it in Navbar

---
+++ src/features/AvatarDropdown/ui/AvatarDropdown.tsx
@@ -0,0 +1,62 @@
+import { FC, memo, useCallback } from 'react';
+import { classNames } from 'shared/lib/classnames/classNames';
+import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Dropdown } from 'shared/ui/Popups';
+import {
+    getUserAuthData,
+    isUserAdmin,
+    isUserManager,
+    userActions,
+} from 'entities/User';
+import { useSelector, useDispatch } from 'react-redux';
+
+interface AvatarDropdownProps {
+    className?: string;
+}
+
+export const AvatarDropdown: FC<AvatarDropdownProps> = memo(
+    (props: AvatarDropdownProps) => {
+        const { className } = props;
+        const { t } = useTranslation();
+        const dispatch = useDispatch();
+        const authData = useSelector(getUserAuthData);
+        const isAdmin = useSelector(isUserAdmin);
+        const isManager = useSelector(isUserManager);
+        const isAdminPanelAvailable = isAdmin || isManager;
+
+        const onLogout = useCallback(() => {
+            dispatch(userActions.logout());
+        }, [dispatch]);
+
+        if (!authData) {
+            return null;
+        }
+
+        return (
+            <Dropdown
+                className={classNames('', {}, [className])}
+                items={[
+                    ...(isAdminPanelAvailable
+                        ? [
+                            {
+                                content: t('Admin Panel'),
+                                href: RoutePath.admin_panel,
+                            },
+                        ]
+                        : []),
+                    {
+                        content: t('Profile'),
+                        href: RoutePath.profile + authData.id,
+                    },
+                    { content: t('Logout'), onClick: onLogout },
+                ]}
+                trigger={
+                    <Avatar src={authData.avatar} size={30} alt={`avatar`} />
+                }
+                direction={`bottom left`}
+            />
+        );
+    }
+);
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -5,23 +5,15 @@ import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classnames/classNames';
 import { Button, ButtonTheme } from 'shared/ui/Button/ui/Button';
 import cls from './Navbar.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-    getUserAuthData,
-    getUserRoles,
-    isUserAdmin,
-    isUserManager,
-    userActions,
-} from 'entities/User';
+import { useSelector } from 'react-redux';
+import { getUserAuthData } from 'entities/User';
 import { Text, TextTheme } from 'shared/ui/Text/ui/Text';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
-import { Dropdown } from 'shared/ui/Popups/ui/Dropdown/Dropdown';
-import { Avatar } from 'shared/ui/Avatar/Avatar';
+
 import { HStack } from 'shared/ui/Stack';
-import NotificationIcon from 'shared/assets/icons/notification-20-20.svg';
-import { Icon } from 'shared/ui/Icon/Icon';
-import { Popover } from 'shared/ui/Popups';
+import { NotificationButton } from 'features/NotificationButton';
+import { AvatarDropdown } from 'features/AvatarDropdown';
 
 interface NavbarProps {
     className?: string;
@@ -29,13 +21,7 @@ interface NavbarProps {
 export const Navbar = memo(({ className }: NavbarProps) => {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
-
     const authData = useSelector(getUserAuthData);
-    const dispatch = useDispatch();
-
-    const isAdmin = useSelector(isUserAdmin);
-
-    const isManager = useSelector(isUserManager);
 
     const onCloseModal = useCallback(() => {
         setIsAuthModal(false);
@@ -44,13 +30,6 @@ export const Navbar = memo(({ className }: NavbarProps) => {
     const onShowModal = useCallback(() => {
         setIsAuthModal(true);
     }, []);
-
-    const onLogout = useCallback(() => {
-        dispatch(userActions.logout());
-        setIsAuthModal(false);
-    }, [dispatch]);
-
-    const isAdminPanelAvailable = isAdmin || isManager;
 
     if (authData) {
         return (
@@ -68,45 +47,8 @@ export const Navbar = memo(({ className }: NavbarProps) => {
                     {t('create new article')}
                 </AppLink>
                 <HStack gap={`16`} className={cls.actions}>
-                    <Popover
-                        direction={'bottom left'}
-                        trigger={
-                            <Button theme={ButtonTheme.CLEAR}>
-                                <Icon
-                                    Svg={NotificationIcon}
-                                    inverted={true}
-                                ></Icon>
-                            </Button>
-                        }
-                    >
-                        test
-                    </Popover>
-
-                    <Dropdown
-                        items={[
-                            ...(isAdminPanelAvailable
-                                ? [
-                                    {
-                                        content: t('Admin Panel'),
-                                        href: RoutePath.admin_panel,
-                                    },
-                                ]
-                                : []),
-                            {
-                                content: t('Profile'),
-                                href: RoutePath.profile + authData.id,
-                            },
-                            { content: t('Logout'), onClick: onLogout },
-                        ]}
-                        trigger={
-                            <Avatar
-                                src={authData.avatar}
-                                size={30}
-                                alt='avatar'
-                            />
-                        }
-                        direction={`bottom left`}
-                    />
+                    <NotificationButton />
+                    <AvatarDropdown />
                 </HStack>
             </header>
         );
