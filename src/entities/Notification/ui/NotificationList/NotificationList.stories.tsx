4fec65c245aecc8d4853dbd7e04cad8ad7a23d5b
Aika C <aika.carriere@gmail.com>
2023-08-16 11:40:15 -0500

    Create Notification entity
    - created Notification enitity, api and NotificationItem and NotificationList components

---
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { NotificationList } from './NotificationList';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+
+export default {
+    title: 'shared/NotificationList',
+    component: NotificationList,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof NotificationList>;
+
+
+
+const Template: ComponentStory<typeof NotificationList> = (args) => (
+    <NotificationList {...args} />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
