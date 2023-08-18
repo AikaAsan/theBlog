5695afc8a02acbf5508dbdab0bc58bfaf8f3603e
Aika C <aika.carriere@gmail.com>
2023-08-18 13:00:24 -0500

    edit Drawer component to use Spring and Gesture libraries through useAnimationLibs hook "

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,10 +1,11 @@
-import { Modes, classNames } from 'shared/lib/classnames/classNames';
-import cls from './Drawer.module.scss';
-import { ReactNode } from 'react';
+import { classNames } from 'shared/lib/classnames/classNames';
+import { ReactNode, memo, useCallback, useEffect } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Overlay } from '../Overlay/Overlay';
-import { useModal } from 'shared/lib/hooks/useModal/useModal';
-import { Portal } from '@headlessui/react';
+import { Portal } from '../Portal/Portal';
+import { useAnimationLibs } from 'shared/components/AnimationProvider';
+import cls from './Drawer.module.scss';
+
 interface DrawerProps {
     className?: string;
     children: ReactNode;
@@ -13,38 +14,107 @@ interface DrawerProps {
     lazy?: boolean;
 }
 
-export const Drawer = (props: DrawerProps) => {
+const height = window.innerHeight - 100;
+
+export const DrawerContent = memo((props: DrawerProps) => {
     const { className, children, isOpen, onClose, lazy } = props;
 
     const { theme } = useTheme();
-    const { close, isClosing, isMounted } = useModal({
-        animationDelay: 300,
-        onClose,
-        isOpen,
-    });
+    const { Spring, Gesture } = useAnimationLibs();
 
-    const modes: Modes = {
-        [cls.opened]: isOpen,
-        [cls.isClosing]: isClosing,
+    const [{ y }, api] = Spring.useSpring(() => ({ y: height }));
+
+    const openDrawer = useCallback(() => {
+        api.start({ y: 0, immediate: false });
+    }, [api]);
+
+    useEffect(() => {
+        if (isOpen) {
+            openDrawer();
+        }
+    }, [api, isOpen, openDrawer]);
+
+    const close = (velocity = 0) => {
+        api.start({
+            y: height,
+            immediate: false,
+            config: { ...Spring.config.stiff, velocity },
+            onResolve: onClose,
+        });
     };
 
-    if (lazy && !isMounted) {
+    const bind = Gesture.useDrag(
+        ({
+            last,
+            velocity: [, vy],
+            direction: [, dy],
+            movement: [, my],
+            cancel,
+        }) => {
+            if (my < -70) cancel();
+
+            if (last) {
+                if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+                    close();
+                } else {
+                    openDrawer();
+                }
+            } else {
+                api.start({ y: my, immediate: true });
+            }
+        },
+        {
+            from: () => [0, y.get()],
+            filterTaps: true,
+            bounds: { top: 0 },
+            rubberband: true,
+        }
+    );
+
+    if (!isOpen) {
         return null;
     }
+
+    const display = y.to((py) => (py < height ? 'block' : 'none'));
 
     return (
         <Portal>
             <div
-                className={classNames(cls.Drawer, modes, [
+                className={classNames(cls.Drawer, {}, [
                     className,
                     theme,
                     'app_drawer',
                 ])}
             >
                 <Overlay onClick={close}>
-                    <div className={cls.content}>{children}</div>
+                    <Spring.a.div
+                        className={cls.sheet}
+                        style={{
+                            display,
+                            bottom: `calc(-100vh + ${height - 100}px)`,
+                            y,
+                        }}
+                        {...bind()}
+                    >
+                        {children}
+                    </Spring.a.div>
                 </Overlay>
             </div>
         </Portal>
     );
-};
+});
+
+export const Drawer = memo((props: DrawerProps) => {
+    const { className, children, isOpen, onClose, lazy } = props;
+    const { isLoaded } = useAnimationLibs();
+
+    if (!isLoaded) {
+        return null;
+    }
+
+    return (
+        <DrawerContent isOpen={isOpen} onClose={onClose} lazy>
+            {children}
+        </DrawerContent>
+    );
+});
