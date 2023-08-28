7ffbd093f208347f6deb9d28e337808ceedb1c07
Aika C <aika.carriere@gmail.com>
2023-08-28 13:34:22 -0500

    created StarRating component in shared/ui layer, added star icon, updated Icon component

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -2,14 +2,14 @@ import { FC, memo } from 'react';
 import { classNames } from '@/shared/lib/classnames/classNames';
 import cls from './Icon.module.scss';
 
-interface IconProps {
+interface IconProps extends React.SVGProps<SVGSVGElement> {
     className?: string;
     Svg: React.VFC<React.SVGProps<SVGSVGElement>>;
     inverted?: boolean;
 }
 
-export const Icon: FC<IconProps> = memo((props: IconProps) => {
-    const { className , Svg, inverted} = props;
+export const Icon = memo((props: IconProps) => {
+    const { className , Svg, inverted, ...otherProps } = props;
 
-    return <Svg className={classNames(inverted ? cls.inverted: cls.icon, {}, [className])} />;
+    return <Svg className={classNames(inverted ? cls.inverted : cls.icon, {}, [className])} {...otherProps} />;
 });
---
+++ src/shared/ui/StarRating/StarRating.tsx
@@ -0,0 +1,84 @@
+import { FC, memo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from '@/shared/lib/classnames/classNames';
+import cls from './StarRating.module.scss';
+import { Icon } from '../Icon/Icon';
+import star from '@/shared/assets/icons/star.svg';
+interface StarRatingProps {
+    className?: string;
+    onSelect?: (starsCount: number) => void;
+    size?: number;
+    selectedStars?: number;
+}
+
+const stars = [1, 2, 3, 4, 5];
+
+export const StarRating: FC<StarRatingProps> = memo(
+    (props: StarRatingProps) => {
+        const { className, onSelect, size = 30, selectedStars = 0 } = props;
+        const { t } = useTranslation();
+
+        const [currentStarsCount, setCurrentStarsCount] = useState(0);
+        const [isSelected, setIsSelected] = useState(Boolean(selectedStars));
+
+        //used closure to close starCount var in order to keep it in context
+        const onHover = (starCount: number) => () => {
+            if (!isSelected) {
+                setCurrentStarsCount(starCount);
+            }
+        };
+
+        const onLeave = () => {
+            if (!isSelected) {
+                setCurrentStarsCount(0);
+            }
+        };
+
+        const onClick = (starsCount: number) => () => {
+            // if (!isSelected) {
+            //     onSelect?.(starsCount);
+            //     setCurrentStarsCount(starsCount);
+            //     setIsSelected(true);
+            // }
+            console.log('starCount in OnClick', starsCount);
+            if (starsCount === 1) {
+                onSelect?.(starsCount);
+                setCurrentStarsCount(starsCount);
+                setIsSelected(!isSelected);
+            }
+            if (!isSelected && starsCount > 1) {
+                onSelect?.(starsCount);
+                setCurrentStarsCount(starsCount);
+                setIsSelected(true);
+            }
+        };
+
+        console.log('currentStarCount', currentStarsCount);
+        console.log('IsSelected', isSelected);
+
+        return (
+            <div className={classNames(cls.starRating, {}, [className])}>
+                {stars.map((starNumber) => (
+                    <Icon
+                        Svg={star}
+                        key={starNumber}
+                        className={classNames(
+                            cls.starIcon,
+                            { [cls.selected]: isSelected },
+                            [
+                                currentStarsCount >= starNumber
+                                    ? cls.hovered
+                                    : cls.normal,
+                            ]
+                        )}
+                        width={size}
+                        height={size}
+                        onMouseEnter={onHover(starNumber)}
+                        onMouseLeave={onLeave}
+                        onClick={onClick(starNumber)}
+                    />
+                ))}
+            </div>
+        );
+    }
+);
